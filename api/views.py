0c20f3cd57f5d87e30bb89c50bd8b55bd065755e
yyt <2298269347@qq.com>
2022-08-31 14:54:26 +0800

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+# Create your views here.
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        print(request.data)
+        return Response({"status": True})
