cc54f805d68b0abec098323fa9b446d9c0844241
yyt <2298269347@qq.com>
2022-09-04 23:48:39 +0800

    drf练习

--- api/views.py
+++ api/views.py
@@ -1,11 +1,126 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.filters import BaseFilterBackend
+from rest_framework import serializers
+from django.forms import model_to_dict
 from rest_framework.response import Response
 import re
 import random
 import uuid
 from api.Serializer.account import MessageSerializer, LoginSerializer
 from api import models
+
+
+class NewsModelSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    topic = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.News
+        fields = ['id', 'cover', 'content', 'topic', "user", 'favor_count']
+
+    def get_user(self, obj):
+        # return {'id': obj.user_id, 'nickname': obj.user.nickname, 'avatar': obj.user.avatar}
+        return model_to_dict(obj.user, fields=['id', 'nickname', 'avatar'])
+
+    def get_topic(self, obj):
+        if not obj.topic:
+            return
+        # return {'id': obj.topic_id, 'title': obj.topic.title}
+        return model_to_dict(obj.topic, fields=['id', 'title'])
+
+
+"""
+class NewsView(APIView):
+    def get(self, request, *args, **kwargs):
+        min_id = request.query_params.get('min_id')
+        max_id = request.query_params.get('max_id')
+        if min_id:
+            queryset = models.News.objects.filter(id__lt=min_id).order_by('-id')[0:10]
+        elif max_id:    
+            queryset = models.News.objects.filter(id__gt=max_id).order_by('id')[0:10]
+        else:
+            queryset = models.News.objects.all().order_by('-id')[0:10]
+        ser = NewsModelSerializer(instance=queryset, many=True)
+        return Response(ser.data, status=200)
+"""
+
+
+############################## 动态列表 #############################
+
+class NewsPagination(LimitOffsetPagination):
+    default_limit = 5
+    max_limit = 50
+    limit_query_param = 'limit'
+    offset_query_param = 'offset'
+
+    def get_offset(self, request):
+        return 0
+
+    def get_paginated_response(self, data):
+        return Response(data)
+
+
+class MinFilterBackend(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        min_id = request.query_params.get('min_id')
+        if min_id:
+            return queryset.filter(id__lt=min_id).order_by('-id')
+        return queryset
+
+
+class MaxFilterBackend(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        max_id = request.query_params.get('max_id')
+        if max_id:
+            return queryset.filter(id__gt=max_id).order_by('id')
+        return queryset
+
+
+class NewsView(ListAPIView):
+    serializer_class = NewsModelSerializer
+    queryset = models.News.objects.all().order_by('-id')
+    pagination_class = NewsPagination
+    filter_backends = [MinFilterBackend, MaxFilterBackend]
+
+
+# ############################# 话题  #############################
+class TopicModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Topic
+        fields = "__all__"
+
+
+class TopicView(ListAPIView):
+    serializer_class = TopicModelSerializer
+    queryset = models.Topic.objects.all().order_by('-id')
+
+    pagination_class = NewsPagination
+    filter_backends = [MinFilterBackend, MaxFilterBackend]
+
+
+############################## 动态详细 #############################
+
+class NewsDetailModelSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.News
+        # fields = "__all__"
+        exclude = ['cover', ]
+
+    def get_images(self, obj):
+        detail_queryset = models.NewsDetail.objects.filter(news=obj)
+        # return [row.cos_path for row in detail_queryset]
+        # return [{'id':row.id,'path':row.cos_path} for row in detail_queryset]
+        return [model_to_dict(row, ['id', 'cos_path']) for row in detail_queryset]
+
+
+class NewsDetailView(RetrieveAPIView):
+    serializer_class = NewsDetailModelSerializer
+    queryset = models.News.objects
 
 
 # Create your views here.
@@ -66,7 +181,7 @@ class MessageView(APIView):
         random_code = random.randint(1000, 9999)
         # 4.验证码发送到手机上，购买服务器进行发送短信：腾讯云
         # 略
-        print("随机嘛",random_code)
+        print("随机嘛", random_code)
         # 5.把验证码+手机号保留（30s过期） conn.set("15944445555","1234",ex=40)
         from django_redis import get_redis_connection
         conn = get_redis_connection()
--- auction/urls.py
+++ auction/urls.py
@@ -4,6 +4,9 @@ from api import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('login/',views.LoginView.as_view()),
-    path('message/',views.MessageView.as_view()),
+    path('login/', views.LoginView.as_view()),
+    path('message/', views.MessageView.as_view()),
+    path('news/', views.NewsView.as_view()),
+    path('topic/', views.TopicView.as_view()),
+    path('news/<int:pk>/', views.NewsDetailView.as_view()),
 ]
